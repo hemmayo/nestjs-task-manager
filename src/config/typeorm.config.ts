b423b56ea5afae263d545d0f06eb6c7445aa46ac
Emmanuel Popoola <olaayo10@gmail.com>
2020-06-21 14:01:33 +0100

    configure typeorm

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'ruby.db.elephantsql.com',
   port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'taskmanager',
+  username: 'zqbzdgmb',
+  password: 'sswHJudNqrvFM1b8gDwdGWcAgdnpBWE4',
+  database: 'zqbzdgmb',
   entities: [__dirname + '/../**/*.entity.ts'],
   synchronize: true,
 };
